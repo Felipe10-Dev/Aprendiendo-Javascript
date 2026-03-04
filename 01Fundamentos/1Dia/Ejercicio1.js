da0f06cbec79c1d3db93bbf581ff724112b45e3c
Felipe10-Dev <felipeandresjimenes1734@gmail.com>
2026-03-04 15:45:38 -0500

    Practica Variables Y Tipos De Datos

---
+++ 01Fundamentos/1Dia/Ejercicio1.js
@@ -0,0 +1,20 @@
+// Definimos Las Variables
+let nombre = "Felipe"; 
+let edad = 20; 
+let ciudad = "Yopal" 
+
+
+
+// Definimos Variable Donde Va Todo El Saludo ES6
+let saludo = `Hola, me llamo ${nombre} tengo ${edad} y vivo en ${ciudad}.`; 
+
+// Imprimimos El Saludo 
+console.log(saludo); 
+
+
+
+
+
+
+
+
