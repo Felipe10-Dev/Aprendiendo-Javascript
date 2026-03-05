76629fe65f3f740210fb35b7157cab0fe7e7dc86
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 15:27:01 -0500

    Practica Validacion De Numero Par O Impar

---
+++ 01Fundamentos/2Dia/2Reto.js
@@ -0,0 +1,16 @@
+// Programa Que Valida Si Un Numero Es Par O Impar
+let numero = 10; 
+
+
+// Validamos Si Es Par O Impar
+if (numero % 2 === 0) {
+
+    console.log("Numero Par"); 
+
+} else {
+
+    console.log("Numero Impar"); 
+
+}
+
+
