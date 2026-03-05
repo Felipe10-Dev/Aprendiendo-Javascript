eb9e610459c2301463cd18a95ec2f4011ca09fe4
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 15:24:00 -0500

    Practica Validacion De Edad

---
+++ 01Fundamentos/2Dia/1Reto.js
@@ -0,0 +1,25 @@
+// Validar Si Una Persona Es Mayor O Menor De Edad
+
+
+
+// Definimos Las Variable
+let edad = 15; 
+
+
+// Validamos Si La Persona Es Mayor O Menor De Edad
+if (edad >= 18) {
+
+    console.log("Mayor De Edad"); 
+
+} else if (edad < 18 && edad >= 0) {
+
+    console.log("Menor De Edad"); 
+
+} else {
+
+    console.log("Edad Invalida"); 
+
+}
+
+
+
