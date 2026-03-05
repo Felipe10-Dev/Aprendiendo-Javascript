973877ab02f0d9d99522bd99b59659554992f2c8
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 14:06:44 -0500

    Practica Conversion De Datos

---
+++ 01Fundamentos/1Dia/2Reto.js
@@ -0,0 +1,17 @@
+// Conversion De Tipos 
+
+
+
+
+// String A Numero
+let numberText = "100"; 
+
+console.log(Number(numberText) + 100); // Suma 
+
+console.log(parseInt(numberText) + 100); // Suma Otra Forma De Conversion
+
+
+// Number A String
+let number = 200.0; 
+
+console.log(String(number) + 100) // Concatena 
