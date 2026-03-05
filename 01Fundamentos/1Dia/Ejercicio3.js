ce2ba103193b3c63986e0e2ff96e26a73ff6c151
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-05 14:15:07 -0500

    Modificacion De Nombres

--- 01Fundamentos/1Dia/Ejercicio3.js
+++
@@ -1,9 +0,0 @@
-// Definimos Las Variables
-let name = "Andres Felipe";
-let lastname = "Jimenez Hernandez";
-
-// Definimos La Variable Saludo Concatenada De La Nueva Forma ES6 <- Templates Literals
-let saludo = `Hola, ${name} ${lastname}`; 
-
-// Imprimimos
-console.log(saludo); 
