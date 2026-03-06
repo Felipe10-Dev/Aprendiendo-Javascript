9b2f4be80b1b4c7c7d304aeb58c92edbaab7d920
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 13:22:17 -0500

    Practica Validar Si Una Persona Esta En Regla Para Conducir

---
+++ 01Fundamentos/3Dia/4Reto.js
@@ -0,0 +1,19 @@
+// Definimos Las Variables
+let edad = 20; 
+let hasLicence = true; 
+
+
+
+// Mediante Una Sentencia De Control Mostramos Si Puedo O No Conducir Y Si Tiene La Edad Requerida
+if (edad >= 18 && hasLicence === true) {
+
+    console.log("Puede Conducir"); 
+
+} else {
+
+    console.log("No Puedes Conducir"); 
+
+}
+
+
+
