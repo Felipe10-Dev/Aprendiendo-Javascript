134874162e22d227414d4f6791a1b480d39c7b28
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 13:19:13 -0500

    Validar Si Un Numero Es Positivo O Negativo

---
+++ 01Fundamentos/3Dia/1Reto.js
@@ -0,0 +1,16 @@
+// Definimos Las Variables
+let numero = 10; 
+
+
+// Mediante Una Sentencia Condicional Validamos Si El Numero Es Positivo O Negativo
+if (numero > 0) {
+
+    console.log("Numero Positivo"); 
+
+} else {
+
+    console.log("Numero Negativo"); 
+
+}
+
+
