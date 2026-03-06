e3af146c8eb983c3695c371d49e38fd25f040c87
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 13:20:02 -0500

    Practica Validar Si Una Persona Es Mayor De Edad

---
+++ 01Fundamentos/3Dia/2Reto.js
@@ -0,0 +1,14 @@
+// Definimos Las Variables
+let edad = 20; 
+
+// Mediante Una Sentencia De Control Validamos Si La Person Es Mayor De Edad
+if (edad >= 18) {
+
+    console.log("Eres Mayor De Edad");
+
+} else {
+
+    console.log("Eres Menor De Edad");
+
+}
+
