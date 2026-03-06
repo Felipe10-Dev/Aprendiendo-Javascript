2868941b1179f69e527bd9ab446019ba71451a78
Felipe10-Dev <felipinhojimenez1734@gmail.com>
2026-03-06 13:21:26 -0500

    Practica Validar En Que Rango Esta La Nota De Un Respectivo Estudiante

---
+++ 01Fundamentos/3Dia/3Reto.js
@@ -0,0 +1,17 @@
+// Definimos Variables
+let nota = 75; 
+
+// Mediante Una Sentencia De Control Validamos Cual Es El Rango En El Que Se Encuentra La Calificacion De Un Estudiante
+if (nota >= 90) {
+
+    console.log("Excelente"); 
+
+} else if (nota >= 70) {
+
+    console.log("Aprobaste"); 
+
+} else {
+
+    console.log("Perdiste La Materia"); 
+
+}
